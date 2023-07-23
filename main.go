3f7a3462edaa6f17a95c9d814ba9c18674acea3f
Taylor Monacelli <taylormonacelli@gmail.com>
2023-07-23 10:01:06 -0700

    abspath
    path
    
    Revert "path"
    
    This reverts commit 3a77e788be9a3ba9659a44c85a708eb2ff80c628.
    
    Change path to tarsnip in plist to abs path

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-// TerraformOutput is used to unmarshal the JSON output of terraform command
+// TerraformOutput is used to unmarshal the JSON output of the terraform command
 type TerraformOutput struct {
 	InstancePublicIP struct {
 		Value string `json:"value"`
@@ -68,172 +68,27 @@ const PlistTemplate = `
 </plist>
 `
 
-func main() {
-	// Remove timestamp from log output
-	log.SetFlags(0)
+// Generate data/bash_history/summary.txt that contains the unique list of bash lines
+func generateSummaryFile(logDir string) {
+	// Get the unique list of bash lines
+	uniqueLines := getUniqueBashLines(logDir)
 
-	// Define and parse the ip, launchd, label, and cwd flags
-	ipPtr := flag.String("ip", "", "IP address to use instead of running Terraform")
-	launchdPtr := flag.Bool("launchd", false, "Create launchd .plist file")
-	labelPtr := flag.String("label", "com.tarsnap", "The label for the .plist file")
-	cwdPtr := flag.String("cwd", ".", "Working directory for the launchd task")
-	showFullPtr := flag.Bool("show-full", false, "Show the unique list of lines to stdout")
-	flag.Parse()
-
-	var absCwd string
-
-	// If --show-full flag is provided, only show the unique list of bash lines
-	if *showFullPtr {
-		logDir := "./data/bash_history"
-		uniqueLines := getUniqueBashLines(logDir)
-		for _, line := range uniqueLines {
-			fmt.Println(line)
-		}
-		return
-	}
-
-	// Expand cwd into an absolute path
-	absCwd, err := filepath.Abs(*cwdPtr)
+	// Create or open the summary.txt file
+	summaryFile, err := os.Create(filepath.Join(logDir, "summary.txt"))
 	if err != nil {
-		log.Fatalf("Failed to get absolute path: %v", err)
+		log.Fatalf("Failed to create summary.txt: %v", err)
 	}
+	defer summaryFile.Close()
 
-	var ip string
-
-	// Check if the ip flag was set
-	if *ipPtr != "" {
-		log.Println("Using provided IP address...")
-		ip = *ipPtr
-	} else {
-		log.Println("Running Terraform command to get output...")
-
-		out, err := exec.Command("terraform", "-chdir=./terraform", "output", "-json").Output()
+	// Write the unique lines to the summary file
+	for _, line := range uniqueLines {
+		_, err := fmt.Fprintln(summaryFile, line)
 		if err != nil {
-			log.Fatalf("Failed to execute command: %v", err)
+			log.Fatalf("Failed to write to summary.txt: %v", err)
 		}
-
-		log.Println("Parsing JSON output...")
-
-		var tfOutput TerraformOutput
-		err = json.Unmarshal(out, &tfOutput)
-		if err != nil {
-			log.Fatalf("Failed to parse JSON: %v", err)
-		}
-
-		ip = tfOutput.InstancePublicIP.Value
 	}
 
-	// Create a launchd .plist file if the launchd flag is set
-	if *launchdPtr {
-		log.Println("Creating launchd .plist file...")
-
-		tmpl, err := template.New("plist").Parse(PlistTemplate)
-		if err != nil {
-			log.Fatalf("Failed to create template: %v", err)
-		}
-
-		exePath, err := os.Executable()
-		if err != nil {
-			panic(err)
-		}
-
-		exeName := filepath.Base(exePath)
-		fmt.Println(exeName)
-
-		// Here's where we change the filename and label logic
-		filename := fmt.Sprintf("%s.%s.plist", *labelPtr, ip)
-
-		data := PlistData{
-			Label:   filename[:len(filename)-6], // trim .plist extension
-			IP:      ip,
-			Args:    exePath,
-			Path:    os.Args[0],
-			Cwd:     absCwd,
-			LogPath: fmt.Sprintf("/tmp/%s.log", os.Args[0]),
-		}
-
-		file, err := os.Create(filename)
-		if err != nil {
-			log.Fatalf("Failed to create .plist file: %v", err)
-		}
-		defer file.Close()
-
-		err = tmpl.Execute(file, data)
-		if err != nil {
-			log.Fatalf("Failed to execute template: %v", err)
-		}
-
-		log.Println("Successfully created launchd .plist file.")
-		return
-	}
-
-	log.Println("Copying remote bash history file to local machine...")
-
-	// Set SSH user
-	user := "root"
-
-	// Create local directory if it does not exist
-	localDir := "./data/bash_history"
-	err = os.MkdirAll(localDir, 0o755)
-	if err != nil {
-		log.Fatalf("Failed to create directory: %v", err)
-	}
-
-	// Append current timestamp to the filename
-	localFile := fmt.Sprintf("%s/bash_history_%s.txt", localDir, time.Now().Format("20060102_150405"))
-	absLocalFile, err := filepath.Abs(localFile)
-	if err != nil {
-		log.Fatalf("Failed to get absolute path: %v", err)
-	}
-
-	// Execute the scp command to copy the remote bash history file to the local machine
-	cmd := exec.Command("scp", fmt.Sprintf("%s@%s:~/.bash_history", user, ip), absLocalFile)
-	log.Println("Executing command:", cmd.String()) // Logging the command
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("Failed to execute command: %v", err)
-	}
-
-	log.Println("Successfully copied remote bash history file to the local machine.")
-
-	log.Println("Output from the scp command:")
-	log.Println(string(out))
-
-	// Loop over all the files in the data/bash_history directory
-	log.Println("Summary of data files:")
-	lineCounts := make(map[string]int)
-	aggregateLines := []string{}
-
-	err = filepath.Walk(localDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			// Only consider regular files
-			fileLines, lines, err := readLines(path)
-			if err != nil {
-				return err
-			}
-			lineCounts[path] = fileLines
-			aggregateLines = append(aggregateLines, lines...)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatalf("Failed to walk through files: %v", err)
-	}
-
-	// Display the summary of data files
-	for path, count := range lineCounts {
-		log.Printf("File: %s, Line Count: %d", path, count)
-	}
-
-	// Get the unique line count for the aggregate of all files
-	uniqueLineCount := getUniqueLineCount(aggregateLines)
-	log.Printf("Unique Line Count for Aggregate of All Files: %d", uniqueLineCount)
-
-	log.Println("Finished.")
+	log.Println("Successfully generated summary.txt.")
 }
 
 // readLines reads all lines from a file and returns the line count and slice of lines
@@ -310,4 +165,184 @@ func getUniqueBashLines(logDir string) []string {
 	}
 
 	return lines
+}
+
+func main() {
+	// Remove timestamp from log output
+	log.SetFlags(0)
+
+	// Define and parse the ip, launchd, label, and cwd flags
+	ipPtr := flag.String("ip", "", "IP address to use instead of running Terraform")
+	launchdPtr := flag.Bool("launchd", false, "Create launchd .plist file")
+	labelPtr := flag.String("label", "com.tarsnap", "The label for the .plist file")
+	cwdPtr := flag.String("cwd", ".", "Working directory for the launchd task")
+	showFullPtr := flag.Bool("show-full", false, "Show the unique list of lines to stdout")
+	flag.Parse()
+
+	var absCwd string
+
+	// If --show-full flag is provided, only show the unique list of bash lines
+	if *showFullPtr {
+		logDir := "./data/bash_history"
+		uniqueLines := getUniqueBashLines(logDir)
+		for _, line := range uniqueLines {
+			fmt.Println(line)
+		}
+		return
+	}
+
+	// Expand cwd into an absolute path
+	absCwd, err := filepath.Abs(*cwdPtr)
+	if err != nil {
+		log.Fatalf("Failed to get absolute path: %v", err)
+	}
+
+	var ip string
+
+	// Check if the ip flag was set
+	if *ipPtr != "" {
+		log.Println("Using provided IP address...")
+		ip = *ipPtr
+	} else {
+		log.Println("Running Terraform command to get output...")
+
+		out, err := exec.Command("terraform", "-chdir=./terraform", "output", "-json").Output()
+		if err != nil {
+			log.Fatalf("Failed to execute command: %v", err)
+		}
+
+		log.Println("Parsing JSON output...")
+
+		var tfOutput TerraformOutput
+		err = json.Unmarshal(out, &tfOutput)
+		if err != nil {
+			log.Fatalf("Failed to parse JSON: %v", err)
+		}
+
+		ip = tfOutput.InstancePublicIP.Value
+	}
+
+	// Create a launchd .plist file if the launchd flag is set
+	if *launchdPtr {
+		log.Println("Creating launchd .plist file...")
+
+		tmpl, err := template.New("plist").Parse(PlistTemplate)
+		if err != nil {
+			log.Fatalf("Failed to create template: %v", err)
+		}
+
+		exePath, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+
+		absExePath, err := filepath.Abs(exePath)
+		if err != nil {
+			panic(err)
+		}
+
+		exeDir := filepath.Dir(absExePath)
+		fmt.Println("Executable Path:", absExePath)
+		fmt.Println("Executable Directory:", exeDir)
+
+		exeName := filepath.Base(exePath)
+		fmt.Println(exeName)
+
+		// Here's where we change the filename and label logic
+		filename := fmt.Sprintf("%s.%s.plist", *labelPtr, ip)
+
+		data := PlistData{
+			Label:   filename[:len(filename)-6], // trim .plist extension
+			IP:      ip,
+			Args:    absExePath,
+			Path:    exeDir,
+			Cwd:     absCwd,
+			LogPath: fmt.Sprintf("/tmp/%s.log", os.Args[0]),
+		}
+
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatalf("Failed to create .plist file: %v", err)
+		}
+		defer file.Close()
+
+		err = tmpl.Execute(file, data)
+		if err != nil {
+			log.Fatalf("Failed to execute template: %v", err)
+		}
+
+		log.Println("Successfully created launchd .plist file.")
+		return
+	}
+
+	log.Println("Copying remote bash history file to the local machine...")
+
+	// Set SSH user
+	user := "root"
+
+	// Create local directory if it does not exist
+	localDir := "./data/bash_history"
+	err = os.MkdirAll(localDir, 0o755)
+	if err != nil {
+		log.Fatalf("Failed to create directory: %v", err)
+	}
+
+	// Append current timestamp to the filename
+	localFile := fmt.Sprintf("%s/bash_history_%s.txt", localDir, time.Now().Format("20060102_150405"))
+	absLocalFile, err := filepath.Abs(localFile)
+	if err != nil {
+		log.Fatalf("Failed to get absolute path: %v", err)
+	}
+
+	// Execute the scp command to copy the remote bash history file to the local machine
+	cmd := exec.Command("scp", fmt.Sprintf("%s@%s:~/.bash_history", user, ip), absLocalFile)
+	log.Println("Executing command:", cmd.String()) // Logging the command
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to execute command: %v", err)
+	}
+
+	log.Println("Successfully copied remote bash history file to the local machine.")
+
+	log.Println("Output from the scp command:")
+	log.Println(string(out))
+
+	// Loop over all the files in the data/bash_history directory
+	log.Println("Summary of data files:")
+	lineCounts := make(map[string]int)
+	aggregateLines := []string{}
+
+	err = filepath.Walk(localDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			// Only consider regular files
+			fileLines, lines, err := readLines(path)
+			if err != nil {
+				return err
+			}
+			lineCounts[path] = fileLines
+			aggregateLines = append(aggregateLines, lines...)
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to walk through files: %v", err)
+	}
+
+	// Display the summary of data files
+	for path, count := range lineCounts {
+		log.Printf("File: %s, Line Count: %d", path, count)
+	}
+
+	// Get the unique line count for the aggregate of all files
+	uniqueLineCount := getUniqueLineCount(aggregateLines)
+	log.Printf("Unique Line Count for Aggregate of All Files: %d", uniqueLineCount)
+
+	log.Println("Finished.")
+
+	// Generate summary.txt file containing unique list of bash lines
+	generateSummaryFile(localDir)
 }
